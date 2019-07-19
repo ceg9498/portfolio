737b62bdc6a2ae59cc8559643850a24952d21604
Emily Gagne <ceg9498@gmail.com>
2019-07-19 16:26:50 -0400

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-## Portfolio
-
-This is the project for my Portfolio, which shows things I've made. It's [currently available for viewing on GitHub Pages](https://ceg9498.github.io/portfolio/).
-
-I originally made this as one of the projects for [freeCodeCamp](https://www.freecodecamp.org/)'s Responsive Web Design certification, but I liked it so much I decided to keep it.
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ precache-manifest.35331e28f4bcfbbbf6aff1dd0a21282c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8bfdec613679bdb859d7c2e17cb476a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "05adba6d12f5a2c3785e",
+    "url": "/portfolio/static/css/main.34a7112b.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "05adba6d12f5a2c3785e",
+    "url": "/portfolio/static/js/main.90a66b93.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
