3d485095a10c051b123d57a75b5611c32ed76ede
C.E. Gagne <ceg9498@gmail.com>
2019-08-21 12:37:52 -0400

    Updates

---
+++ precache-manifest.a799f7b333067cd623050fb87d8bafc7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744a16e78c4170f30b58d657eca253c0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3d468686d070ce2eb3bd",
+    "url": "/portfolio/static/css/main.a7c158e6.chunk.css"
+  },
+  {
+    "revision": "8bad699c2014ce99b4b5",
+    "url": "/portfolio/static/js/2.4c7363d6.chunk.js"
+  },
+  {
+    "revision": "3d468686d070ce2eb3bd",
+    "url": "/portfolio/static/js/main.291d4b58.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "fdbe6ed29fa9579051cf104f47e4e27b",
+    "url": "/portfolio/static/media/jsCalculator.fdbe6ed2.png"
+  },
+  {
+    "revision": "74661f1fe8059bf720c1512fece8c6be",
+    "url": "/portfolio/static/media/markdownPreviewer.74661f1f.png"
+  },
+  {
+    "revision": "d857f11120bf7709dc50fd2052048806",
+    "url": "/portfolio/static/media/pomodoroTimer.d857f111.png"
+  },
+  {
+    "revision": "6879cb7f15e89b8c05f67d4efaccef0c",
+    "url": "/portfolio/static/media/quoteMachine.6879cb7f.png"
+  },
+  {
+    "revision": "00857240d4f7964c61e0d4df2fb29f6f",
+    "url": "/portfolio/static/media/techDocPage.00857240.png"
+  },
+  {
+    "revision": "ee180aee3db1acd9b661e36ffef2e8ca",
+    "url": "/portfolio/static/media/translationPractice.ee180aee.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.cce893e93b2d3d8ea851e43a46b5dbee.js"
+  "/portfolio/precache-manifest.a799f7b333067cd623050fb87d8bafc7.js"
 );
 
 self.addEventListener('message', (event) => {
