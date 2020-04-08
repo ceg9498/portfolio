7371d71510fd47495baaf0b6fd97b8f8f8390e37
C.E. Gagne <ceg9498@gmail.com>
2020-04-08 14:11:17 -0400

    Update the skills section
    Now includes skill cards, woohoo.

--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {SkillCard as Card} from './Card'
+import color from '../data/techColors'
+import skills from '../data/skills.json'
 import styles from './Skills.module.css'
 
-export function Skills() {
-  return(
-    <section className={styles.skills} id="#skills">
-      <h2 className="hidden">Skills</h2>
-    </section>
-  )
+export class Skills extends React.PureComponent {
+	render() {
+		return(
+			<section id="skills" className={styles.skills}>
+				<h2 className="hidden">Skills</h2>
+				<div className={styles.deck}>
+					{skills.map((item)=>
+						<Card
+							title={item.title}
+							color={color.get(item.title)}
+							info={item.info}
+						/>
+					)}
+				</div>
+			</section>
+		)
+	}
 }
