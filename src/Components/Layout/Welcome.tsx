d3c811197ef54dad6e54547504b0ca2040846cc0
Emily Gagne <ceg9498@gmail.com>
2020-04-08 17:34:50 -0400

    Add photo

--- src/Components/Layout/Welcome.tsx
+++ src/Components/Layout/Welcome.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './Welcome.module.scss'
 
-// import profile from '../../Images/photo.png'
+import profile from '../../Images/photo.jpg'
 
 export function Welcome(){
   return(
     <header id="welcome-section" className={styles.section}>
       <h1 className={styles.grid}>
         <span className={styles.greeting}>Hi!</span>
-        <div className={styles.photo} /*style={{backgroundImage: `url('${profile}')`}}*/></div>
+        <div className={styles.photo} style={{backgroundImage: `url('${profile}')`}}></div>
         <span className={styles.name}>I'm Emily.</span>
         <span className={styles.tagline}>It's nice to meet you.</span>
       </h1>
