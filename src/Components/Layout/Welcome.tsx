34d68df4c899e2248d45454d7b49d196acea37e0
C.E. Gagne <ceg9498@gmail.com>
2020-04-08 17:02:26 -0400

    Update Welcome css; make space for a photo.

--- src/Components/Layout/Welcome.tsx
+++ src/Components/Layout/Welcome.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import styles from './Welcome.module.scss'
+
+// import profile from '../../Images/photo.png'
 
 export function Welcome(){
   return(
-    <header id="welcome-section">
-      <h1 id="welcome-grid">
-        <span id="w-greeting">Hi!</span>
-        <span id="w-name">I'm Emily.</span>
-        <span id="w-tagline">It's nice to meet you.</span>
+    <header id="welcome-section" className={styles.section}>
+      <h1 className={styles.grid}>
+        <span className={styles.greeting}>Hi!</span>
+        <div className={styles.photo} /*style={{backgroundImage: `url('${profile}')`}}*/></div>
+        <span className={styles.name}>I'm Emily.</span>
+        <span className={styles.tagline}>It's nice to meet you.</span>
       </h1>
     </header>
   );
